a6e42a043b2983ae5c48db7e540d064745a8e3a1
Fabrizio <fabriziospoto1@gmail.com>
2020-07-07 17:03:36 +0200

    Definitivo 2
    Definitivo 2

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ var anni = parseInt(prompt('Quanti anni hai?'));
 
 if (anni < 18) {
     var prezzoMinori = km*0.21*0.8;
-    document.getElementById('prezzo-finale').innerHTML = 'Complimenti, hai uno sconto del 20%. Devi pagare' + ' ' + prezzoMinori + '€' + '.' + ' ' + 'Grazie e buon viaggio!';
+    document.getElementById('prezzo-finale').innerHTML = 'Complimenti, hai uno sconto del 20%. Devi pagare' + ' ' + prezzoMinori.toFixed(2) + '€' + '.' + ' ' + 'Grazie e buon viaggio!';
 } else if (anni > 65) {
     var prezzoOver = km*0.21*0.6;
-    document.getElementById('prezzo-finale').innerHTML = 'Complimenti, hai uno sconto del 40%. Devi pagare' + ' ' + prezzoOver + '€' + '.' + ' ' + 'Grazie e buon viaggio!';
+    document.getElementById('prezzo-finale').innerHTML = 'Complimenti, hai uno sconto del 40%. Devi pagare' + ' ' + prezzoOver.toFixed(2) + '€' + '.' + ' ' + 'Grazie e buon viaggio!';
 } else {
     var prezzoNormale = km*0.21;
-    document.getElementById('prezzo-finale').innerHTML = 'Devi pagare' + ' ' + prezzoNormale + '€' + '.' + ' ' + 'Grazie e buon viaggio!';
+    document.getElementById('prezzo-finale').innerHTML = 'Devi pagare' + ' ' + prezzoNormale.toFixed(2) + '€' + '.' + ' ' + 'Grazie e buon viaggio!';
 }
